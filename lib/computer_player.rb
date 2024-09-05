b0aed4fa7e0a67f8f886bc046d10ec4d09421468
Tclewis13 <the3rdi@gmail.com>
2024-09-05 04:21:09 -0400

    computer uses yellow intelligently!

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,11 +2,12 @@ require 'colorize'
 require_relative 'guess'
 
 class Computer_Player
-  attr_accessor :knowledge
+  attr_accessor :knowledge, :held_color
 
   COLORS = %i[red blue green cyan magenta yellow]
 
   def initialize
+    self.held_color = :white
     self.knowledge = []
     knowledge[0] = :white
     knowledge[1] = :white
@@ -18,6 +19,9 @@ class Computer_Player
     feedback.guess_array.each_with_index do |space, index|
       knowledge[index] = guess.guess_array[index] if space == :blue
     end
+    feedback.guess_array.each_with_index do |space, index|
+      self.held_color = guess.guess_array[index] if knowledge[index] == :white && space == :yellow
+    end
   end
 
   def make_guess(turn)
@@ -27,10 +31,12 @@ class Computer_Player
       comp_guess = Guess.new(%i[white white white white], turn)
       knowledge.each_with_index do |space, index|
         comp_guess.guess_array[index] = space if space != :white
+        comp_guess.guess_array[index] = held_color if comp_guess.guess_array[index] == :white && held_color != :white
       end
       comp_guess.guess_array.each_with_index do |space, index|
         comp_guess.guess_array[index] = COLORS.sample if space == :white
       end
+      self.held_color = :white
       comp_guess
     end
   end
--- lib/feedback.rb
+++ lib/feedback.rb
@@ -11,7 +11,7 @@ class Feedback < Guess
   def calculate_feedback(guess, code_guess) # rubocop:disable Metrics/MethodLength
     working_code = code_guess.guess_array.clone
     guess.guess_array.each_with_index do |color, index|
-      guess_array << if working_code.include? color
+      guess_array << if working_code[0..index].include? color
                        if code_guess.guess_array[index] == color
                          working_code[index] = :white
                          :blue
